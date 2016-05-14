7b5b3e26debd942225f756ff404e154e833280c5
mrahmani1373 <mrahmani1373@gmail.com>
2016-05-14 22:11:31 +0430

    status change first draft

--- association/urls.py
+++ association/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^news/$', 'association.views.news'),
     url(r'^notices/$', 'association.views.notices'),
     url(r'requests/$', 'association.views.requests'),
-    url(r'deleteRequest/$', 'association.views.delete_new'),
+    url(r'deleteRequest/$', 'association.views.delete_request'),
+    url(r'saveChanges/$', 'association.views.save_changes'),
     url(r'^$', 'association.views.home'),
 ]
--- association/views.py
+++ association/views.py
@@ -45,21 +45,38 @@ def requests(request):
     })
 def delete_request(request):
     if request.method == "POST":
-        print("true")
-        list_of_delete = request.POST.getlist('new_del_inputs')
-        for del_id in list_of_delete:
-            CoopApplication.objects.all().filter(id=del_id).update(is_deleted=True)
-        list_of_delete = request.POST.getlist('rec_del_inputs')
-        for del_id in list_of_delete:
-            CoopApplication.objects.all().filter(id=del_id).update(is_deleted=True)
-        list_of_delete = request.POST.getlist('rev_del_inputs')
-        for del_id in list_of_delete:
-            CoopApplication.objects.all().filter(id=del_id).update(is_deleted=True)
-        list_of_delete = request.POST.getlist('del_rest_inputs')
-        for del_id in list_of_delete:
-            CoopApplication.objects.all().filter(id=del_id).update(is_deleted=False)
+        temp = request.POST.getlist('which-button')
+        for value in temp:
+            if value == '1':
+                list_of_changes= request.POST.getlist('status')
+                print(list_of_changes)
+                list_of_ids = request.POST.getlist('app-id')
+                print(list_of_ids)
+                counter =0
+                for id in list_of_ids:
+                    CoopApplication.objects.all().filter(id=id).update(status = list_of_changes[counter])
+                    counter+=1
+            else:
+                list_of_delete = request.POST.getlist('new_del_inputs')
+                for del_id in list_of_delete:
+                    if del_id != "":
+                        CoopApplication.objects.all().filter(id=del_id).update(is_deleted=True)
+                list_of_delete = request.POST.getlist('rec_del_inputs')
+                for del_id in list_of_delete:
+                    if del_id != "":
+                        CoopApplication.objects.all().filter(id=del_id).update(is_deleted=True)
+                list_of_delete = request.POST.getlist('rev_del_inputs')
+                for del_id in list_of_delete:
+                    if del_id != "":
+                        CoopApplication.objects.all().filter(id=del_id).update(is_deleted=True)
+                list_of_delete = request.POST.getlist('del_rest_inputs')
+                for del_id in list_of_delete:
+                    if del_id != "":
+                        CoopApplication.objects.all().filter(id=del_id).update(is_deleted=False)
     return HttpResponseRedirect('/association/requests/')
 
-@register.filter(name='lookup')
-def cut(value, arg):
-    return value[arg]
+def save_changes(request):
+    print('ye baby')
+    if request.method == "POST":
+        temp = request.POST.getlist('which-button')
+        print(temp)
